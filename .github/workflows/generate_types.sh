25c9d80f982390c55da82ca6498bfca967ea5af0
Aaron Imming <aaim@protonmail.com>
2020-12-30 16:48:20 +0800

    DEV

--- .github/workflows/generate_types.sh
+++ .github/workflows/generate_types.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/binary-com/websockets.git
+cd websockets
 git checkout HEAD
 npm i json-schema-to-typescript
 ./node_modules/.bin/json2ts --input "./config/v3/**/[!example]*.json"
